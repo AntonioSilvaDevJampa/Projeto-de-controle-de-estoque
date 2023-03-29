05b8eb9c8f8d2448f8878323436d30cfbf18de56
AntonioSilvaDev <111434678+AntonioSilvaDevJampa@users.noreply.github.com>
2023-03-29 11:20:57 +0000

    eliminei algumas linhas de codigo de comentarios e acrescentei mais 4 linhas sendo uma de comentario e outras três linhas de codigo.

--- main.py
+++ main.py
@@ -24,17 +24,9 @@ formatted_datetime = datetime_brasilia.strftime("%d-%m-%Y %H:%M:%S")
 
 print("Data e hora no fuso horário do Brasil:", formatted_datetime)
 
-#now = datetime.now()
-
-# formatar a hora no formato brasileiro (dd/mm/yyy hh:mm:ss)
-
-#br_time = now.strftime("%d/%m/%Y %H:%M:%S")
-
 ## Usar o imput para registrar a data ##
 
 data_entrada = input(f' digite a data de hoje ')
-
-data_hora = datetime.strptime(data_entrada, '%d/%m/%Y')
 
 print(f' A data de hoje é , {data_entrada}')
 
@@ -53,6 +45,43 @@ print(f' a quantidade do produto é de {quantidade}')
 data_val = input( 'Digite a data de validade do produto ')
 
 print(f' A data de validade do produto é , {data_val}')
+
+# agora calcular a diferença de data de entrada e a data de validade, transformar as variaveis data_entrada e data_val em variaveis do tipo datetime
+
+data_entrada = datetime.strptime(data_entrada,"%d/%m/%Y")
+
+data_val = datetime.strptime(data_val,"%d/%m/%Y")
+
+print( data_val - data_entrada)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
